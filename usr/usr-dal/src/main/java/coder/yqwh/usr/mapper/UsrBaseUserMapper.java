1ef4764b70e06bb8147d55031b7eafeeba598496
hejie <coderyqwh@163.com>
2020-12-18 23:32:27 +0800

    微服务集成

--- usr/usr-dal/src/main/java/coder/yqwh/usr/mapper/UsrBaseUserMapper.java
+++
@@ -1,7 +0,0 @@
-package coder.yqwh.usr.mapper;
-
-import coder.yqwh.usr.model.UsrBaseUser;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UsrBaseUserMapper extends Mapper<UsrBaseUser> {
-}
